0d46929885c80321052b44bb3c717a57de26f443
Igor432 <igomar0602@gmail.com>
2022-03-16 18:05:48 +0200

    linear added

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/* hamburger */
+
+
 const hamburger = document.getElementById('hamburger');
 const navUl = document.getElementById('nav-ul');
 const hiddenCon = document.getElementById('hidden-con')
@@ -6,3 +9,34 @@ hamburger.addEventListener('click', () => {
     navUl.classList.toggle('show');
 
 })
+
+
+
+/* SLIDER */
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
