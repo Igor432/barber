0acf82755f8c3163710a12af500fe437ad9477b3
Igor432 <igomar0602@gmail.com>
2022-03-17 11:44:53 +0200

    color change

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 /* hamburger */
 
+const hamburger = document.getElementById("hamburger");
+const navUl = document.getElementById("nav-ul");
+const hiddenCon = document.getElementById("hidden-con");
 
-const hamburger = document.getElementById('hamburger');
-const navUl = document.getElementById('nav-ul');
-const hiddenCon = document.getElementById('hidden-con')
-
-hamburger.addEventListener('click', () => {
-    navUl.classList.toggle('show');
-
-})
-
-
+hamburger.addEventListener("click", () => {
+    navUl.classList.toggle("show");
+});
 
 /* SLIDER */
 
@@ -18,19 +14,23 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
